3c4c0cc213c05e56fb95e88e545872039b48e1f5
Mason Turner <masonturner@ufl.edu>
2013-12-17 15:14:42 -0500

    PID: Move ports and timers to configuration values

--- avr/epiphany/hwlib/pid.cpp
+++ avr/epiphany/hwlib/pid.cpp
@@ -5,6 +5,20 @@
 // Configuration Values
 static TC0_t &pid_tick_timer = TCC0;
 #define PID_TICK_OVF TCC0_OVF_vect
+
+static PORT_t &wheelPort1 = PORTE;
+static TC0_t &wheel1Timer = TCE0;
+static TC1_t &wheel2Timer = TCE1;
+static PORT_t &wheelPort2 = PORTD;
+static TC0_t &wheel3Timer = TCD0;
+static TC1_t &wheel4Timer = TCD1;
+
+#define PID_Wheel1_Event_Channel EVSYS_CHMUX_PORTE_PIN0_gc
+#define PID_Wheel2_Event_Channel EVSYS_CHMUX_PORTE_PIN1_gc
+#define PID_Wheel3_Event_Channel EVSYS_CHMUX_PORTD_PIN0_gc
+#define PID_Wheel4_Event_Channel EVSYS_CHMUX_PORTD_PIN1_gc
+
+
 static const uint16_t pid_tick_timer_period = 32000; //Will generate an interrupt every 1ms
 static const uint8_t AVG_ARRAY_LENGTH = 5;
 static const float CCA_TO_RAD_MULTIPLIER = 6770.674;
@@ -35,45 +49,45 @@ static pid_wheel_data wheelData[4];
 
 void pid_init() {	
 	// Initialize motor frequency measurement timers.
-	PORTE.DIRCLR = 0x02;
-	PORTE.PIN0CTRL = PORT_ISC_BOTHEDGES_gc;
-	TCE0.CTRLA = TC_CLKSEL_DIV64_gc;
-	TCE0.CTRLB = TC0_CCAEN_bm;
-	TCE0.CTRLC = 0x00;
-	TCE0.CTRLD = TC_EVACT_FRQ_gc | TC_EVSEL_CH0_gc;
-	TCE0.CTRLE = 0x00;
-	TCE0.INTCTRLB = TC0_CCAINTLVL1_bm;
-	EVSYS.CH0MUX = EVSYS_CHMUX_PORTE_PIN0_gc;
+	wheelPort1.DIRCLR = 0x02;
+	wheelPort1.PIN0CTRL = PORT_ISC_BOTHEDGES_gc;
+	wheel1Timer.CTRLA = TC_CLKSEL_DIV64_gc;
+	wheel1Timer.CTRLB = TC0_CCAEN_bm;
+	wheel1Timer.CTRLC = 0x00;
+	wheel1Timer.CTRLD = TC_EVACT_FRQ_gc | TC_EVSEL_CH0_gc;
+	wheel1Timer.CTRLE = 0x00;
+	wheel1Timer.INTCTRLB = TC0_CCAINTLVL1_bm;
+	EVSYS.CH0MUX = PID_Wheel1_Event_Channel;
 
-	PORTE.PIN1CTRL = PORT_ISC_BOTHEDGES_gc;
+	wheelPort1.PIN1CTRL = PORT_ISC_BOTHEDGES_gc;
 	TCE1.CTRLA = TC_CLKSEL_DIV64_gc;
 	TCE1.CTRLB = TC1_CCAEN_bm;
 	TCE1.CTRLC = 0x00;
 	TCE1.CTRLD = TC_EVACT_FRQ_gc | TC_EVSEL_CH1_gc;
 	TCE1.CTRLE = 0x00;
 	TCE1.INTCTRLB = TC1_CCAINTLVL1_bm;
-	EVSYS.CH1MUX = EVSYS_CHMUX_PORTE_PIN1_gc;
+	EVSYS.CH1MUX = PID_Wheel2_Event_Channel;
 	
-	PORTD.DIRCLR = 0x02;
-	PORTD.PIN0CTRL = PORT_ISC_BOTHEDGES_gc;
-	TCD0.CTRLA = TC_CLKSEL_DIV64_gc;
-	TCD0.CTRLB = TC0_CCAEN_bm;
-	TCD0.CTRLC = 0x00;
-	TCD0.CTRLD = TC_EVACT_FRQ_gc | TC_EVSEL_CH2_gc;
-	TCD0.CTRLE = 0x00;
-	TCD0.INTCTRLB = TC1_CCAINTLVL1_bm;
-	EVSYS.CH2MUX = EVSYS_CHMUX_PORTD_PIN0_gc;
+	wheelPort2.DIRCLR = 0x02;
+	wheelPort2.PIN0CTRL = PORT_ISC_BOTHEDGES_gc;
+	wheel3Timer.CTRLA = TC_CLKSEL_DIV64_gc;
+	wheel3Timer.CTRLB = TC0_CCAEN_bm;
+	wheel3Timer.CTRLC = 0x00;
+	wheel3Timer.CTRLD = TC_EVACT_FRQ_gc | TC_EVSEL_CH2_gc;
+	wheel3Timer.CTRLE = 0x00;
+	wheel3Timer.INTCTRLB = TC1_CCAINTLVL1_bm;
+	EVSYS.CH2MUX = PID_Wheel3_Event_Channel;
 
-	PORTD.PIN1CTRL = PORT_ISC_BOTHEDGES_gc;
-	TCD1.CTRLA = TC_CLKSEL_DIV64_gc;
-	TCD1.CTRLB = TC1_CCAEN_bm;
-	TCD1.CTRLC = 0x00;
-	TCD1.CTRLD = TC_EVACT_FRQ_gc | TC_EVSEL_CH3_gc;
-	TCD1.CTRLE = 0x00;
-	TCD1.INTCTRLB = TC1_CCAINTLVL1_bm;
-	EVSYS.CH3MUX = EVSYS_CHMUX_PORTD_PIN1_gc;
+	wheelPort2.PIN1CTRL = PORT_ISC_BOTHEDGES_gc;
+	wheel4Timer.CTRLA = TC_CLKSEL_DIV64_gc;
+	wheel4Timer.CTRLB = TC1_CCAEN_bm;
+	wheel4Timer.CTRLC = 0x00;
+	wheel4Timer.CTRLD = TC_EVACT_FRQ_gc | TC_EVSEL_CH3_gc;
+	wheel4Timer.CTRLE = 0x00;
+	wheel4Timer.INTCTRLB = TC1_CCAINTLVL1_bm;
+	EVSYS.CH3MUX = PID_Wheel4_Event_Channel;
 	
-	// Initialize the 1ms PID tick timer
+	// Initialize the PID tick timer
 	pid_tick_timer.CTRLA = TC_CLKSEL_DIV1_gc;	//Uses timer on portC since all timers on PORT E and D will be used for wheels.
 	pid_tick_timer.CTRLB = 0x00;
 	pid_tick_timer.CTRLC = 0x00;
