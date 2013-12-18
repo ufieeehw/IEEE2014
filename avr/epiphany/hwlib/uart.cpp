ec60812c21e20a362f1dc713951c5319280cccf9
Khaled Hassan <khaledjhassan@ufl.edu>
2013-12-18 14:04:26 -0500

    uart: Implement communcation protocol (first workings, anyway)
    TODO: Remove old commented-out code from previous uart.cpp implementation, brush up on comm.txt and other bits of documentation, check for buffer overrun conditions and other safety hazards

--- avr/epiphany/hwlib/uart.cpp
+++ avr/epiphany/hwlib/uart.cpp
@@ -1,8 +1,5 @@
 #include "uart.h"
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdio.h>
-#include <string.h>
+
 
 // Configuration Values
 static USART_t &uart_usb = USARTC0;
@@ -13,19 +10,32 @@ static const uint8_t txpin_usb = 3;
 static const uint8_t rxpin_usb = 2;
 static const uint16_t bsel_usb = 3301;		// 19200 baud
 static const uint8_t bscale_usb = 0b1011;
-static bool echo_enabled = true;
+//static bool echo_enabled = true;
+static const uint8_t max_message_len = 64;
+static const char start_char = '^';
+// flags
+static bool waiting_for_msg_ACK = false;
+static bool no_ACK_after_3x = false;
+static uint8_t start_char_count = 0;
+static uint8_t rx_message_len = 0;
+static uint8_t tx_message_len = 0;
+static RxStates_t rx_state = WaitingForStart;
+bool message_to_be_handled = false;
 
+
+/*
 // Stdio Parameters
 static int put(char ch, FILE* file);
 static int get(FILE* file);
 static FILE stdinout;
+*/
 
 // Buffer to contain data from UART.
 struct UARTData {
-	char outbuf[64];
-	volatile uint8_t outbuf_pos;
-	char inbuf[8];
-	volatile uint8_t inbuf_pos;
+	char buffer[max_message_len];
+	volatile uint8_t buf_pos;
+//	char inbuf[8];
+//	volatile uint8_t inbuf_pos;
 };
 
 static UARTData uart_data_usb;
@@ -41,9 +51,9 @@ void uart_init() {
 	uart_usb.BAUDCTRLA = bsel_usb & 0xFF;
 	uart_usb.BAUDCTRLB = (bscale_usb << USART_BSCALE_gp) | (bsel_usb >> 8);
 	
-	fdev_setup_stream(&stdinout, put, get, _FDEV_SETUP_RW);
-	stdin = &stdinout;
-	stdout = &stdinout;
+//	fdev_setup_stream(&stdinout, put, get, _FDEV_SETUP_RW);
+//	stdin = &stdinout;
+//	stdout = &stdinout;
 	
 }
 
@@ -51,16 +61,17 @@ void uart_init() {
 // so that the transmit ISR will be hit.
 bool uart_put(char ch) {
 	// First check to make sure that the buffer pointer isn't pointing to the end of the buffer already.
-	if (uart_data_usb.outbuf_pos >= sizeof(uart_data_usb.outbuf))
+	if (uart_data_usb.buf_pos >= sizeof(uart_data_usb.buffer))
 		return false;
 	
 	// Since it's not, we'll make sure the DREIF IF is disabled, so that we're not interrupted while adding data to the buffer and updating the pointer location.
 	uart_usb.CTRLA &= ~USART_DREINTLVL_gm;
-	uart_data_usb.outbuf[uart_data_usb.outbuf_pos++] = ch;
+	uart_data_usb.buffer[uart_data_usb.buf_pos++] = ch;
 	uart_usb.CTRLA |= USART_DREINTLVL_MED_gc; // Re-enable the interrupt, so that it'll transmit what was just placed in teh buffer
 	return true;
 }
 
+/*
 // Gets a character from the 0th position in the inbuf.
 int uart_get() {
 	// First make sure there's stuff actually in the buffer.  If there isn't then return -1, so we can tell in the function that is called.
@@ -75,35 +86,140 @@ int uart_get() {
 	
 	return ch;
 }
-
+*/
 // Gets a character from the data register in the uart and puts it into the input buffer.
+
 static void receive() {
+	cli();
+	
 	// First get the data from the UART.
 	uint8_t byte = uart_usb.DATA;
 	
-	if (uart_data_usb.inbuf_pos >= sizeof(uart_data_usb.inbuf))
-		return;
+	switch(rx_state){
+		case WaitingForStart:
+			if(message_to_be_handled == false){
+				if(byte==start_char){
+					start_char_count++;
+				}
+			
+				if(start_char_count == 3){
+					rx_state = GrabLength;
+					start_char_count = 0;
+				}
+			}
+		break;
+
+		case GrabLength:
+			rx_message_len = byte;
+			uart_data_usb.buf_pos = 0;
+			rx_state = ReceivingMessage;
+			//CRCInit();
+		break;
+		
+		case ReceivingMessage:
+			if(waiting_for_msg_ACK == true){
+				if(byte == ACKValid){
+					waiting_for_msg_ACK = false;
+					rx_state = WaitingForStart;
+					break;
+				}
+				else if(byte == ACKInvalid){
+					//retransmit();
+					break;
+				}	
+			}
+			
+			
+			else if(rx_message_len-- > 0){
+				uart_data_usb.buffer[uart_data_usb.buf_pos++] = byte;
+			}else {
+				message_to_be_handled = true;
+				rx_state = WaitingForStart;
+			}
+		break;
+		
+		default:
+		break;
 	
-	uart_data_usb.inbuf[uart_data_usb.inbuf_pos++] = byte;
+	}
+	
+	sei();
+//	uart_data_usb.inbuf[uart_data_usb.inbuf_pos++] = byte;
 }
 
 // Gets a character from the output buffer and puts it into uart to be transmitted
 static void transmit() {
-	// First check if there's actually data in the buffer.
-	if(uart_data_usb.outbuf_pos > 0) {
-		// If there is, then send the first character in the buffer.
-		uart_usb.DATA = uart_data_usb.outbuf[0];
-		uart_data_usb.outbuf_pos--;
+	// First check if there's untransmitted message data in the buffer.
+	if(uart_data_usb.buf_pos < tx_message_len) {
+		// If there is, then send the first untransmitted character in the buffer.
+		uart_usb.DATA = uart_data_usb.buffer[uart_data_usb.buf_pos++];
 		
-		//Check to see if there's still data in the buffer, and if there is, move it forward one position in the buffer.
-		if(uart_data_usb.outbuf_pos > 0)
-			memmove(uart_data_usb.outbuf, uart_data_usb.outbuf+1, uart_data_usb.outbuf_pos);
 	} else {
 		//If there isn't any data in the buffer, then disable the DREIF so that it doesn't continue interrupting.
 		uart_usb.CTRLA &= ~USART_DREINTLVL_gm;
 	}
 }
 
+bool uart_send_msg_noblock(uint8_t type, char* msg){
+	uint8_t len = strlen(msg) + 1; // figure out the message length (all messages are strings... for now...) (plus 1 to account for msg type)
+	
+	tx_message_len = len + 4;
+	if(tx_message_len > max_message_len){
+		return false;
+	}
+	
+	//if anyone has any reason why this transmission should not occur, speak now or forever --ah, you know the rest
+	
+	
+	// we'll make sure the DREIF IF is disabled, so that we're not interrupted while adding data to the buffer and updating the pointer location.
+	uart_usb.CTRLA &= ~USART_DREINTLVL_gm;
+	
+	//fill first 3 bytes of buffer with start character (^)
+	memset(uart_data_usb.buffer, '^', 3);
+	//copy the len(gth) into the next byte of the buffer, 
+	uart_data_usb.buffer[3] = len;
+	//...followed by the type, 
+	uart_data_usb.buffer[4] = type;
+	//...and len-1 bytes of the msg (type + msg = len bytes long)
+	for(int i = 0; i < (len-1); i++){
+		uart_data_usb.buffer[i+5] = *msg; //copy the data at address pointed to by msg into buffer 
+										  //(start at 5 positions after start because
+		msg++;
+	}
+	uart_data_usb.buf_pos = 0; 
+	
+	//len+4 = number of characters from first ^ to null + 1 to make the buf_pos point to the first empty position?
+	
+	//make sure that the TXISR is activated
+	uart_usb.CTRLA |= USART_DREINTLVL_MED_gc; // Re-enable the interrupt, so that it'll transmit what was just placed in teh buffer
+
+	//set the waiting_for_msg_ACK flag
+	waiting_for_msg_ACK = true;
+	
+	
+	return true; 
+}
+
+bool uart_send_msg_block( uint8_t type, char* msg )
+{
+	if(uart_send_msg_noblock(type, msg) == false){
+		return false;
+	}
+	
+	while(waiting_for_msg_ACK){}
+	
+	if(no_ACK_after_3x){
+		return false;
+	}
+	
+	return true;
+}
+
+char* uart_get_msg(void){
+	return (char*)&uart_data_usb.buffer;
+}
+
+/*
 // stdio functions
 static int put(char ch, FILE* file) {
 	if(ch == '\n')
@@ -129,7 +245,7 @@ static int get(FILE* file) {
 
 	return ch;
 }
-
+*/
 
 ISR(TXVEC_USB) {
 	transmit();
--- avr/epiphany/hwlib/uart.h
+++ avr/epiphany/hwlib/uart.h
@@ -1,8 +1,32 @@
 #ifndef UART_H_
 #define UART_H_
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <string.h>
+
 void uart_init();
 bool uart_put(char ch);
 int uart_get();
+bool uart_send_msg_block(uint8_t type, char* msg);
+bool uart_send_msg_noblock(uint8_t type, char* msg);
+
+typedef enum RxStates_enum {
+	WaitingForStart,
+	GrabLength,
+	ReceivingMessage,
+	Processing
+} RxStates_t;
+
+typedef enum MessageTypes_enum {
+	EchoRequest = '?',
+	EchoReply = 0x01,
+	ACKValid = '_',
+	ACKInvalid = 0x03
+}MessageTypes_t;
+
+char* uart_get_msg(void);
+extern bool message_to_be_handled;
 
 #endif /* UART_H_ */
