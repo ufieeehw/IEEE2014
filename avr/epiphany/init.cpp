480f36f3080efc6b545aca95c253b80fe3ec2dfc
Mason Turner <masonturner@ufl.edu>
2014-01-16 21:39:56 -0500

    clock: Moved references to clocks into separate file.  More to be added later.

--- avr/epiphany/init.cpp
+++ avr/epiphany/init.cpp
@@ -4,19 +4,13 @@
 #include "hwlib/uart.h"
 #include "hwlib/pid.h"
 #include "hwlib/motor.h"
+#include "hwlib/clock.h"
 #include "hwlib/twi.h"
 
 void init() {
 	init_clocks();
 	init_modules();
 	init_interrupts();
-}
-
-void init_clocks() {
-	OSC.CTRL = OSC_RC32MEN_bm | OSC_RC2MEN_bm; // enable 32 Mhz clock (while leaving the current 2 Mhz clock enabled)
-	while (!(OSC.STATUS & OSC_RC32MRDY_bm)) { } // wait for it to stabilize
-	CPU_CCP = CCP_IOREG_gc; // enable access to protected registers
-	CLK.CTRL = CLK_SCLKSEL_RC32M_gc; // switch to the 32 Mhz clock
 }
 
 void init_interrupts() {
