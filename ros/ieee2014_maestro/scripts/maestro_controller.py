e098e641c1ed1cb59e00a08e5f2ab9b230b792f7
Mason Turner <masonturner@ufl.edu>
2014-03-05 23:50:42 -0500

    maestro node: Added a node to interface with the pololu maestro.

---
+++ ros/ieee2014_maestro/scripts/maestro_controller.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import division
+
+from ieee2014_maestro.srv import *
+
+import rospy
+import maestro
+import time
+
+class maestroController(object):
+	def __init__(self):
+		self.servo = None
+
+	def change_clip_position(self, clip):
+		if clip.pos == 1:
+			self.servo.setTarget(0,2000)
+		elif clip.pos == 2:
+			self.servo.setTarget(0,4000)
+		elif clip.pos == 3:
+			self.servo.setTarget(0,6000)
+		return ClipPosResponse()	
+
+	def fire_dart(self, status):
+		if status.fire is True:
+			self.servo.setTarget(1,2000)
+			time.sleep(.1)
+			self.servo.setTarget(1,0)
+		return FireDartResponse()
+		
+	def init_maestro_server(self):
+		self.servo = maestro.Controller()
+		rospy.init_node('maestro_server')
+		test1 = rospy.Service('change_clip_position',ClipPos, self.change_clip_position)
+		test2 = rospy.Service('fire_dart', FireDart, self.fire_dart)
+		self.servo.setTarget(1,0)
+		print "Ready to go, sir!"
+		rospy.spin()
+
+test = maestroController()
+test.init_maestro_server()
+
+rospy.spin()
