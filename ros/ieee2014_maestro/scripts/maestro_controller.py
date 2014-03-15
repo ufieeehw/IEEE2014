38689326352171de37e0fc7ed07ce7f07aedfc43
Forrest Voight <forrest@forre.st>
2014-03-15 11:12:53 -0400

    recalibrated servo

--- ros/ieee2014_maestro/scripts/maestro_controller.py
+++ ros/ieee2014_maestro/scripts/maestro_controller.py
@@ -11,17 +11,21 @@ class maestroController(object):
 	def __init__(self):
 		self.servo = None
 
+	def change_clip_raw_position(self, req):
+		self.servo.setTarget(0, req.pos)
+		return ClipRawPosResponse()	
+
 	def change_clip_position(self, clip):
 		if clip.pos == 1:
-			self.servo.setTarget(0,3350)
+			self.servo.setTarget(0,2600)
 		elif clip.pos == 2:
-			self.servo.setTarget(0,6400)
+			self.servo.setTarget(0,5700)
 		elif clip.pos == 3:
-			self.servo.setTarget(0,10000)
+			self.servo.setTarget(0,8800)
 		elif clip.pos == 4:
-			self.servo.setTarget(0,1600)
+			self.servo.setTarget(0,1000)
 		elif clip.pos == 5:
-			self.servo.setTarget(0,(6400+10000)//2)
+			self.servo.setTarget(0,(5700+8800)//2)
 		return ClipPosResponse()	
 
 	def fire_dart(self, status):
@@ -37,6 +41,7 @@ class maestroController(object):
 		rospy.init_node('maestro_server')
 		test1 = rospy.Service('change_clip_position',ClipPos, self.change_clip_position)
 		test2 = rospy.Service('fire_dart', FireDart, self.fire_dart)
+		test3 = rospy.Service('change_clip_raw_position',ClipRawPos, self.change_clip_raw_position)
 		self.servo.setTarget(1,100)
 		print "Ready to go, sir!"
 		rospy.spin()
